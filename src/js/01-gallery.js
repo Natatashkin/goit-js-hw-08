854364d99f538f71115fa634d8ad5b2c44f92951
Natatashkin <cs070283sng@gmail.com>
2021-10-27 14:51:19 +0300

    update task 1 via 2, add task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,4 @@
-// Описан в документации
 import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Add imports above this line
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,7 +11,7 @@ function onGetTimePlayer() {
       localStorage.setItem(LOCALSTORAGE_KEY, seconds);
     })
     .catch(function (error) {
-      console.log(error);
+      console.log(error.message);
     });
 }
 
@@ -20,35 +20,5 @@ player.on('timeupdate', throttle(onGetTimePlayer, 1000));
 const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
 player.setCurrentTime(currentTime).catch(function (error) {
-  console.log(error);
+  console.log(error.message);
 });
-
-// localStorage.setItem('ui-theme', 'light');
-// localStorage.setItem('sidebar', 'expanded');
-// localStorage.setItem('notification-level', 'mute');
-
-// const settings = {
-//   theme: 'dark',
-//   isAuthenticated: true,
-//   options: [1, 2, 3],
-// };
-
-// localStorage.setItem('settings', JSON.stringify(settings));
-
-// localStorage.setItem('ui-theme', 'dark');
-
-// const theme = localStorage.getItem('ui-theme');
-// console.log(theme); // "dark"
-
-// const savedSettings = localStorage.getItem('settings');
-// console.log(savedSettings);
-// const parsedSettings = JSON.parse(savedSettings);
-// console.log(parsedSettings); // settings objec
-
-// localStorage.setItem('ui-theme', 'light');
-// console.log(localStorage.getItem('ui-theme')); // "dark"
-
-// localStorage.removeItem('ui-theme');
-// console.log(localStorage.getItem('ui-theme')); // null
-
-// localStorage.clear();
